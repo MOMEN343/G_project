3411812da8bced331e42fe16c625dd40595d68dc
mohammed al shanti <mohammedrajab826@gmail.com>
2026-02-01 21:37:53 +0200

    رفع جميع التعديلات على كل الكلاسات

--- admin.py
+++ admin.py
@@ -8,6 +8,7 @@ from PyQt5.QtWidgets import QHBoxLayout
 from PyQt5.QtWidgets import QHeaderView
 from PyQt5.QtCore import Qt
 
+#-------------
 class AdminWindow(QMainWindow):
     def __init__(self):
         super().__init__()
